291e7c69d9fc439665cd8657b4e0716cc0c161c5
Ernest Loveland <edg3@users.noreply.github.com>
2018-03-13 21:23:30 +0200

    split draw/sets/game functionality

---
+++ src/morabHalfDone/DataSets.fs
@@ -0,0 +1,82 @@
+﻿module DataSets
+
+open System
+
+type Position = { 
+    x : int
+    y : int }
+
+
+type Mill = {
+    PointA : Position
+    PointB : Position
+    PointC : Position
+
+    Activ : bool
+    Relodid : bool 
+    } 
+
+
+let flatboard : int list =                     //flat co ordinate system used for operation. See ReadMe
+   // [col0   ;  col1 ;   col2  ;  col3......
+    [0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0]       // .[x co ord][y co ord]. 
+  //[[r1;r2;r3];...]
+
+  
+let mills = //all possible mills                                                                                //////////////////////////////////////////////////////////////////////////////////
+    let a = { PointA ={x= 0;y= 0} ; PointB = {x= 0;y= 8} ; PointC = {x= 0;y= 16} ; Activ = false ; Relodid = true} //verticals up-> down
+    let b = { PointA ={x= 1;y= 0} ; PointB = {x= 1;y= 8} ; PointC = {x= 1;y= 16} ; Activ = false ; Relodid = true} // ''
+    let c = { PointA ={x= 2;y= 0} ; PointB = {x= 2;y= 8} ; PointC = {x= 2;y= 16} ; Activ = false ; Relodid = true} // ''
+    let d = { PointA ={x= 3;y= 0} ; PointB = {x= 3;y= 8} ; PointC = {x= 3;y= 16} ; Activ = false ; Relodid = true}
+    let e = { PointA ={x= 4;y= 0} ; PointB = {x= 4;y= 8} ; PointC = {x= 4;y= 16} ; Activ = false ; Relodid = true}
+    let f = { PointA ={x= 5;y= 0} ; PointB = {x= 5;y= 8} ; PointC = {x= 5;y= 16} ; Activ = false ; Relodid = true}
+    let g = { PointA ={x= 6;y= 0} ; PointB = {x= 6;y= 8} ; PointC = {x= 6;y= 16} ; Activ = false ; Relodid = true}
+    let h = { PointA ={x= 7;y= 0} ; PointB = {x= 7;y= 8} ; PointC = {x= 7;y= 16} ; Activ = false ; Relodid = true}
+    
+    let i = { PointA ={x= 0;y= 0} ; PointB = {x= 1;y= 0} ; PointC = {x= 2;y= 0} ; Activ = false ; Relodid = true} //Horizontal row Left -> right starting left corner 
+    let j = { PointA ={x= 2;y= 0} ; PointB = {x= 3;y= 0} ; PointC = {x= 4;y= 0} ; Activ = false ; Relodid = true}
+    let k = { PointA ={x= 4;y= 0} ; PointB = {x= 5;y= 0} ; PointC = {x= 6;y= 0} ; Activ = false ; Relodid = true}
+    let l = { PointA ={x= 6;y= 0} ; PointB = {x= 7;y= 0} ; PointC = {x= 0;y= 0} ; Activ = false ; Relodid = true}  //Note! loop around     ( 0 ; 0)  
+    
+    let m = { PointA ={x= 0;y= 8} ; PointB = {x= 1;y= 8} ; PointC = {x= 2;y= 8} ; Activ = false ; Relodid = true} // horizontal second row Left -> right
+    let n = { PointA ={x= 2;y= 8} ; PointB = {x= 3;y= 8} ; PointC = {x= 4;y= 8} ; Activ = false ; Relodid = true}
+    let o = { PointA ={x= 4;y= 8} ; PointB = {x= 5;y= 8} ; PointC = {x= 6;y= 8} ; Activ = false ; Relodid = true}
+    let p = { PointA ={x= 6;y= 8} ; PointB = {x= 7;y= 8} ; PointC = {x= 0;y= 8} ; Activ = false ; Relodid = true} //Note! loop around     ( 0 ; 0)  
+
+    let q = { PointA ={x= 0;y= 16} ; PointB = {x= 1;y= 16} ; PointC = {x= 2;y= 16} ; Activ = false ; Relodid = true} // horizontal second row Left -> right
+    let r = { PointA ={x= 2;y= 16} ; PointB = {x= 3;y= 16} ; PointC = {x= 4;y= 16} ; Activ = false ; Relodid = true}
+    let s = { PointA ={x= 4;y= 16} ; PointB = {x= 5;y= 16} ; PointC = {x= 6;y= 16} ; Activ = false ; Relodid = true}
+    let t = { PointA ={x= 6;y= 16} ; PointB = {x= 7;y= 16} ; PointC = {x= 0;y= 16} ; Activ = false ; Relodid = true} //Note! loop around     ( 0 ; 0)  
+    [a;b;c;d;e;f;g;h;i;j;k;l;m;n;o;p;q;r;s;t]
+
+
+
+//FUNCTION DEFINITIONS--------------------------------------------------
+
+let mrbaToFlat (a : string) : int list =           //converts from morabaraba user co ordinates to flat co ordinates
+    match a with
+        |"A1" -> [0;0]            //  Xcoord , Ycoord
+        |"B2" -> [0;8]
+        |"C3" -> [0;16]
+        |"A4" -> [1;0]
+        |"B4" -> [1;8]
+        |"C4" -> [1;16]
+        |"A7" -> [2;0]
+        |"B6" -> [2;8]
+        |"C5" -> [2;16]
+        |"D7" -> [3;0]
+        |"D6" -> [3;8]
+        |"D5" -> [3;16]
+        |"G7" -> [4;0]
+        |"F6" -> [4;8]
+        |"E5" -> [4;16]
+        |"G4" -> [5;0]
+        |"F4" -> [5;8]
+        |"E4" -> [5;16]
+        |"G1" -> [6;0]
+        |"F2" -> [6;8]
+        |"E3" -> [6;16]
+        |"D1" -> [7;0]
+        |"D2" -> [7;8]
+        |"D3" -> [7;16]
+        |_ -> [9;9]
---
+++ src/morabHalfDone/Drawing.fs
@@ -0,0 +1,58 @@
+﻿module Drawing
+
+let rec consColorWrite =
+    fun (msg : string) ->
+        let head = msg.Chars 0
+        let newMsg = msg.Substring(1)
+        match head with
+        | 'X' ->
+            System.Console.ForegroundColor<-System.ConsoleColor.Red
+            System.Console.Write(head);
+        | '0' ->
+            System.Console.ForegroundColor<-System.ConsoleColor.Blue
+            System.Console.Write(head);
+        | '[' | ']' | '-' | '|' | '\\' | '/' | ',' ->
+            System.Console.ForegroundColor<-System.ConsoleColor.DarkGray
+            System.Console.Write(head);
+        | a ->
+            System.Console.ForegroundColor<-System.ConsoleColor.White
+            System.Console.Write(head);
+        match newMsg with
+        | "" -> System.Console.WriteLine() // End line
+        | _ -> consColorWrite newMsg
+
+let whatBoardDraws =
+    fun item ->
+        match item with
+        | 0 -> ' '
+        | 1 -> 'X'
+        | 2 -> '0'
+
+let drawBoard =
+    fun (board: int list) ->
+        let lin00 =         "\t  \t1,2,3    4    5,6,7"
+        let lin01 = sprintf "\t[A]\t%c--------%c--------%c" (whatBoardDraws (board.Item 0)) (whatBoardDraws (board.Item 1)) (whatBoardDraws (board.Item 2))
+        let lin02 =         "\t   \t|\       |       /|"
+        let lin03 = sprintf "\t[B]\t| %c------%c------%c |" (whatBoardDraws (board.Item 8)) (whatBoardDraws (board.Item 9)) (whatBoardDraws (board.Item 10))
+        let lin04 =         "\t   \t| |\     |     /| |"
+        let lin05 = sprintf "\t[C]\t| | %c----%c----%c | |" (whatBoardDraws (board.Item 16)) (whatBoardDraws (board.Item 17)) (whatBoardDraws (board.Item 18))
+        let lin06 =         "\t   \t| | |         | | |"
+        let lin07 = sprintf "\t[D]\t%c-%c-%c         %c-%c-%c" (whatBoardDraws (board.Item 7)) (whatBoardDraws (board.Item 15)) (whatBoardDraws (board.Item 23)) (whatBoardDraws (board.Item 19)) (whatBoardDraws (board.Item 11)) (whatBoardDraws (board.Item 3))
+        let lin08 =         "\t   \t| | |         | | |"
+        let lin09 = sprintf "\t[E]\t| | %c----%c----%c | |" (whatBoardDraws (board.Item 22)) (whatBoardDraws (board.Item 21)) (whatBoardDraws (board.Item 20))
+        let lin10 =         "\t   \t| |/     |     \| |"
+        let lin11 = sprintf "\t[F]\t| %c------%c------%c |" (whatBoardDraws (board.Item 14)) (whatBoardDraws (board.Item 13)) (whatBoardDraws (board.Item 12))
+        let lin12 =         "\t   \t|/       |       \|"
+        let lin13 = sprintf "\t[G]\t%c--------%c--------%c" (whatBoardDraws (board.Item 6)) (whatBoardDraws (board.Item 5)) (whatBoardDraws (board.Item 4))
+        let boundString = "\n\n" + lin00 + "\n\n" + lin01 + "\n" + lin02 + "\n" + lin03 + "\n" 
+                                                  + lin04 + "\n" + lin05 + "\n" + lin06 + "\n"
+                                                  + lin07 + "\n" + lin08 + "\n" + lin09 + "\n" 
+                                                  + lin10 + "\n" + lin11 + "\n" + lin12 + "\n" 
+                                                  + lin13 + "\n";
+        consColorWrite boundString
+
+
+let writeError (msg : string) =
+    System.Console.ForegroundColor<-System.ConsoleColor.Magenta
+    System.Console.WriteLine(msg)
+    System.Threading.Thread.Sleep(1750)
